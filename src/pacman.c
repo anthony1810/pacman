a480696ca363997dc2b724d2c9ca863504eb8077
Anthony Tran <qquang269@gmail.com>
2014-03-24 11:00:17 +0700

    before demonstration

--- src/pacman.c
+++ src/pacman.c
@@ -120,7 +120,7 @@ int main(int argc, char *argv[])
 								stop_command_window(command_window, game_window,map_row,map_col);
 							}else if(strcmp(commands,"wq")==0 && str_recieve[1]== NULL){
 								if(strlen(map_name)>0 && strlen(author)>0 && strlen(author_email)>0){
-									write_to_file(map_name, author, author_email, game_window,map_row, map_col);
+									write_to_file(map_name, author, author_email, game_window,map_col-1, map_row);
 									wprintw(command_window,"write to ");
 									wprintw(command_window, map_name);
 									wprintw(command_window,"and will quit");
@@ -135,7 +135,7 @@ int main(int argc, char *argv[])
 								}														
 							}else if(strcmp(commands,"wq")==0 && str_recieve[1]!= NULL){
 								if(strlen(map_name)>0 && strlen(author)>0 && strlen(author_email)>0){
-									write_to_file(str_recieve[1], author, author_email, game_window,map_row, map_col);
+									write_to_file(str_recieve[1], author, author_email, game_window,map_col-1, map_row);
 									wprintw(command_window,"sucessfully write to ");
 									wprintw(command_window, str_recieve[1]);
 									wprintw(command_window, " and will quit");
@@ -178,7 +178,7 @@ int main(int argc, char *argv[])
 							    wclear(game_window);
 							    wrefresh(game_window);
 							    // delwin(game_window);
-							    game_window=create_new_win(map_row,map_col,GAME_STARTY,0);
+							    game_window=create_new_win(map_row,map_col,GAME_STARTY,(scr_x- map_col)/2);
 							   	readFile(game_window,map_row,map_col,map,s,str_recieve[1]);
 							    fclose(f);
 							    wprintw(command_window,"sucessfully read from ");
@@ -205,15 +205,18 @@ int main(int argc, char *argv[])
 							 	strcat(path,map_name);
 								strcat(path,extension);
 
-								char map[map_row][map_col];
+								char map2[map_row][map_col];
 								// for (int i = 0; i < map_row; i++)
 							 //    {
+							 //    	printw("%d",i);
 							 //        for (int j = 0; i < map_col; j++)
 							 //        {
-							 //            map[i][j]=' ';
+							 //        	printw("%d",j);
+							 //           // map2[i][j]='.';
 							 //        }
 							 //    }
-							    map[0][0]=' ';
+							    refresh();
+							    map2[9][9]=' ';
 								fp=fopen(path, "w");
 
 
@@ -225,14 +228,14 @@ int main(int argc, char *argv[])
 								wclear(game_window);
 							    
 							    // delwin(game_window);
-							    game_window=create_new_win(map_row,map_col,GAME_STARTY,0);
+							    game_window=create_new_win(map_row,map_col,GAME_STARTY,(scr_x- map_col)/2);
 							    wmove(game_window,map_row-1,map_col-2);
 							    refresh();
 							    wrefresh(game_window);
 							    getch();
 								isEnter = 0;
 								stop_command_window(command_window,game_window, map_row, map_col);	
-							    cursorMove(game_window,map_row,map_col,map);
+							    cursorMove(game_window,map_row,map_col,map2);
 								
 								start_command_window(command_window, COMMAND_STARTY);
 
