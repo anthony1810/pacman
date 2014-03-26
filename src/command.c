34b431b377192fabf1c365d4b99175dbb62f493d
Anthony Tran <qquang269@gmail.com>
2014-03-27 00:16:35 +0700

    fixing vi-command, now it's perfect

--- src/command.c
+++ src/command.c
@@ -5,10 +5,8 @@
 #include "write_file.h"
 #include "read_file.h"
 void start_command_window(WINDOW *command_win,int y_cor){
-	cbreak();
 	wclear(command_win);
-	// waddch(command_win,':');
-	wprintw(command_win, "Commands enabled..type enter to begin!");
+	waddch(command_win, ':');
 	move(y_cor,1);
 	refresh();
 	wrefresh(command_win);
@@ -20,8 +18,14 @@ void stop_command_window(WINDOW *command_win,WINDOW *game_window, int y_cor, int
 	wmove(game_window,y_cor-1,x_cor-2);
 	refresh();
 	wrefresh(game_window);
-	noecho();
 }
+
+void refresh_command_window(WINDOW *command_win){
+	wclear(command_win);
+	waddch(command_win, ':');
+	wrefresh(command_win);
+}
+
 
 
 
